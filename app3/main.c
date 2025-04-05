b2ce365def6d9e7532cd15a7f329a522c8e146e6
Nevile8 <142928012+Nevile8@users.noreply.github.com>
2025-04-05 12:59:12 -0300

    Update main.c

--- app3/main.c
+++ app3/main.c
@@ -36,22 +36,29 @@ int read_pizzas(const char *filename, Pizza pizzas[], int *pizza_count) {
 }
 
 
+
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Usage: %s <filename> <command>\n", argv[0]);
+    if (argc < 3) { // Allow at least one command
+        printf("Usage: %s <filename> <command1> [<command2> ...]\n", argv[0]);
         return 1;
     }
 
     const char *filename = argv[1];
-    const char *command = argv[2];
-
     Pizza pizzas[MAX_PIZZAS];
     int pizza_count;
 
-    if (read_pizzas(filename, pizzas, &pizza_count)) {
+    // Read pizzas from the file
+    if (!read_pizzas(filename, pizzas, &pizza_count)) {
+        return 1;
+    }
+
+    // Process each command
+    for (int i = 2; i < argc; i++) {
+        const char *command = argv[i];
+
         if (strcmp(command, "pms") == 0) {
             find_best_sold_pizza(pizzas, pizza_count);
-        } else if (strcmp(command, "pls") == 0) { // New command for worst-sold pizza
+        } else if (strcmp(command, "pls") == 0) {
             find_worst_sold_pizza(pizzas, pizza_count);
         } else {
             printf("Unknown command: %s\n", command);
